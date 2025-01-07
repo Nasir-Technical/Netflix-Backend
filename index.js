f78a529d70dbe56b84354e0195088061a767f2f2
Nasir-Technical <nasir.18.2006@gmail.com>
2025-01-08 04:36:32 +0500

    updeate code

---
+++ index.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import databaseConnection from './utils/database.js';
+import cookieParser from 'cookie-parser';
+import userRoute from "./routes/userRoute.js";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// Load environment variables from .env file
+dotenv.config({ path: './.env' });
+
+// Connect to the database
+databaseConnection();
+
+// Initialize Express app
+const app = express();
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+const corsOptions = {
+    // origin: process.env.FRONT_URL,  // frontend URL
+    origin: 'http://localhost:3000',  // Allow requests from frontend
+    methods: ['GET', 'POST'],  
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+// Resolve __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the React app's build directory
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get("/test", (req, res) => {
+    res.send("Serving...");
+});
+
+// Dummy route for testing
+app.get('/dummy', (req, res) => {
+    res.send('<h1>This is a dummy route</h1>');
+  });
+
+// API Routes
+app.use("/api/v1/user", userRoute);
+
+// Handle any requests that don't match the ones above and serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+// Authentication Middleware
+const isAuthenticated = (req, res, next) => {
+    if (req.cookies.token) {
+        next(); // User is authenticated
+    } else {
+        res.redirect('/'); // User is not authenticated, redirect to login page
+    }
+};
+
+// Protect the /browse route
+app.get('/browse', isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
