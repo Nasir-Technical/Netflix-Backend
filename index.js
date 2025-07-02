258b059fdd138ebae14971f25b1cf6b6cf93252f
Nasir-Technical <nasir.18.2006@gmail.com>
2025-07-02 19:27:41 +0500

    changes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 // CORS
 const corsOptions = {
-    origin: 'https://netflix-backend-three.vercel.app/', // or 3000 depending on frontend
+    origin: ["http://localhost:3000", "https://netflix-frontend-one-chi.vercel.app","https://netflix-backend-three.vercel.app/"], // or 3000 depending on frontend
     methods: ["GET", "POST"],
     credentials: true,
 };
