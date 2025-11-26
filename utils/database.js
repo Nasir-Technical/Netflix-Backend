c97d62980649ae86006663b1aceee76a9553f550
Nasir-Technical <nasir.18.2006@gmail.com>
2025-11-26 15:43:18 +0500

    bug fixing backend

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config({
-    path:"../.env"
-})
+dotenv.config();
 
 const databaseConnection = () => {
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+    mongoose.connect(process.env.MONGO_URL).then(()=>{
         console.log("mongodb connected successfully");
     }).catch((error)=>{
         console.log(error);
