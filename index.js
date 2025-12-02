7e053a43d183ac2cc7741b092d9b59bb55bae195
Nasir-Technical <nasir.18.2006@gmail.com>
2025-12-02 18:09:16 +0500

    remove app.listen for serverless

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ app.get("/api/test", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`✅ Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//     console.log(`✅ Server is running on port ${PORT}`);
+// });
 
-
+export default app;
