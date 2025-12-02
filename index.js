09707a375207515c79335a851011ca271625d2e0
Nasir-Technical <nasir.18.2006@gmail.com>
2025-12-02 21:19:20 +0500

    testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import databaseConnection from '../utils/database.js';
 import cookieParser from 'cookie-parser';
-import userRoute from "../routes/userRoute.js";
+// import userRoute from "../routes/userRoute.js";
 import cors from "cors";
 
 
@@ -37,11 +37,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
-app.use("/api/v1/user", userRoute);
+// app.use("/api/v1/user", userRoute);
 
 // Health check
 app.get("/api/test", (req, res) => {
-    res.send("Backend is working!");
+  res.type('text');
+  res.send("Backend is working!");
 });
 
 // Start server
